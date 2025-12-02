deeb3591411b6f64a844906f152ca074dde825cd
Sanja Daic <sanja.daic86@gmail.com>
2025-12-02 13:21:10 +0100

    Creating and Displaying To-Do List

---
+++ src/components/ToDoList/TodoList.jsx
@@ -0,0 +1,27 @@
+const TodoList = ({ todos }) => {
+  return (
+    <section>
+      <h3>To-Do's</h3>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              name="completed"
+              defaultChecked={todo.completed}
+            />
+            <div>
+              {todo.name}
+              <br />
+              {todo.description}
+              <br />
+              {todo.deadline} {todo.priority !== "none" && todo.priority}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default TodoList;
