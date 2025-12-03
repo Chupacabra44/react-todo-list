bc03c9c45a36cbca456c844f492ade187948dfeb
Sanja Daic <sanja.daic86@gmail.com>
2025-12-03 10:52:03 +0100

    Displaying an Edit Form for To-Do’s

--- src/components/ToDoFormFields/ToDoFormFields.jsx
+++ src/components/ToDoFormFields/ToDoFormFields.jsx
@@ -1,9 +1,9 @@
 import { PRIORITIES, PRIORITY_DEFAULT } from "../../constants/priorities";
 import styles from "./ToDoFormFields.module.css";
 
-const ToDoFormFields = ({ show }) => {
+const ToDoFormFields = ({ todo, show = true }) => {
   return (
-    <div className={styles.FormFields}>
+    <div style={{ width: "100%" }}>
       <div className={styles.FormField}>
         <input
           type="text"
@@ -11,6 +11,7 @@ const ToDoFormFields = ({ show }) => {
           placeholder="Name*"
           name="name"
           autoComplete="off"
+          defaultValue={todo?.name}
         />
       </div>
 
@@ -22,19 +23,25 @@ const ToDoFormFields = ({ show }) => {
               placeholder="Description"
               name="description"
               rows="3"
+              defaultValue={todo?.description}
             />
           </div>
 
           <div className={styles.FormGroup}>
             <div className={styles.FormField}>
               <label htmlFor="deadline">Deadline</label>
-              <input type="date" id="deadline" name="deadline" />
+              <input
+                type="date"
+                id="deadline"
+                name="deadline"
+                defaultValue={todo?.deadline}
+              />
             </div>
 
             <div className={styles.FormField}>
               <label htmlFor="priority">Priority</label>
               <select
-                defaultValue={PRIORITY_DEFAULT}
+                defaultValue={todo?.priority ?? PRIORITY_DEFAULT}
                 id="priority"
                 name="priority"
               >
--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,35 +1,57 @@
+import { useState } from "react";
 import { PRIORITIES, PRIORITY_DEFAULT } from "../../constants/priorities";
+import ToDoFormFields from "../ToDoFormFields/ToDoFormFields";
 import styles from "./TodoListItem.module.css";
 
 const TodoListItem = ({ todo, onUpdate }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const handleCompleted = (event) => {
     onUpdate(todo.id, { ...todo, completed: event.target.checked });
   };
-  return (
-    <li className={styles.TodoListItem} data-completed={todo.completed}>
-      <div className={styles.Content}>
-        <input
-          type="checkbox"
-          name="completed"
-          checked={todo.completed}
-          onChange={handleCompleted}
-          className={styles.Status}
-        />
-        <div className={styles.Info}>
-          {todo.name}
-          {todo.description && (
-            <span className={styles.Description}>{todo.description}</span>
+
+  const viewingTemplate = (
+    <div className={styles.Content}>
+      <input
+        type="checkbox"
+        name="completed"
+        checked={todo.completed}
+        onChange={handleCompleted}
+        className={styles.Status}
+      />
+      <div className={styles.Info}>
+        {todo.name}
+        {todo.description && (
+          <span className={styles.Description}>{todo.description}</span>
+        )}
+        <div className={styles.AdditionalInfo}>
+          {todo.deadline}{" "}
+          {todo.priority !== PRIORITY_DEFAULT && (
+            <span style={{ color: PRIORITIES[todo.priority]?.color }}>
+              {PRIORITIES[todo.priority]?.label}
+            </span>
           )}
-          <div className={styles.AdditionalInfo}>
-            {todo.deadline}{" "}
-            {todo.priority !== PRIORITY_DEFAULT && (
-              <span style={{ color: PRIORITIES[todo.priority]?.color }}>
-                {PRIORITIES[todo.priority]?.label}
-              </span>
-            )}
-          </div>
         </div>
       </div>
+      <div className={styles.Controls}>
+        <button onClick={() => setIsEditing(true)}>📝</button>
+      </div>
+    </div>
+  );
+
+  const editingTemplate = (
+    <form className={styles.Content} onReset={() => setIsEditing(false)}>
+      <ToDoFormFields todo={todo} />
+      <div className={styles.Controls}>
+        <input type="submit" value="💾" />
+        <input type="reset" value="❌" />
+      </div>
+    </form>
+  );
+
+  return (
+    <li className={styles.TodoListItem} data-completed={todo.completed}>
+      {isEditing ? editingTemplate : viewingTemplate}
     </li>
   );
 };
