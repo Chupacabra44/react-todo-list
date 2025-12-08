ed91c016ea3a9e1637d2eaff2142edc173c45964
Sanja Daic <sanja.daic86@gmail.com>
2025-12-08 10:29:22 +0100

    Simplifying HTTP Requests with Axios

--- src/api.js
+++ src/api.js
@@ -1,55 +1,33 @@
-const BASE_URL = import.meta.env.VITE_MOCKAPI_BASE_URL;
+import axios from "axios";
+
+const http = axios.create({
+  baseURL: import.meta.env.VITE_MOCKAPI_BASE_URL,
+  headers: { "content-type": "application/json" },
+  timeout: 5000,
+});
+
+http.interceptors.response.use(({ data }) => data);
 
 export const api = {
   todos: {
     getAll(params = {}) {
-      const searchParams = new URLSearchParams(params).toString();
-      return fetch(`${BASE_URL}todos?${searchParams}`, {
-        method: "GET",
-        headers: { "content-type": "application/json" },
-      }).then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else if (response.status === 404) {
-          return [];
-        }
-      });
+      return http
+        .get("todos", { params })
+        .catch((error) =>
+          error?.response.status === 404 ? [] : Promise.reject(error)
+        );
     },
 
     create(data) {
-      return fetch(`${BASE_URL}todos`, {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        // Send your data in the request body as JSON
-        body: JSON.stringify(data),
-      }).then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-      });
+      return http.post("todos", data);
     },
 
     update(id, data) {
-      return fetch(`${BASE_URL}todos/${id}`, {
-        method: "PUT",
-        headers: { "content-type": "application/json" },
-        // Send your data in the request body as JSON
-        body: JSON.stringify(data),
-      }).then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-      });
+      return http.put(`todos/${id}`, data);
     },
 
     delete(id) {
-      return fetch(`${BASE_URL}todos/${id}`, {
-        method: "DELETE",
-      }).then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-      });
+      return http.delete(`todos/${id}`);
     },
   },
 };
