762e66bd7f8a55f579092a9d959fe628ae900586
Sanja Daic <sanja.daic86@gmail.com>
2025-12-08 11:34:48 +0100

    Handle API Errors with Try...Catch and Async/Await

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,44 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [filters, setFilters] = useState({});
 
-  const fetchTodos = () => {
-    api.todos.getAll(filters).then(setTodos);
+  const fetchTodos = async () => {
+    try {
+      const data = await api.todos.getAll(filters);
+      setTodos(data);
+    } catch (error) {
+      console.log("Failed to get todo's. Please try again.");
+    }
   };
 
   useEffect(() => {
     fetchTodos();
   }, [filters]);
 
-  const handleCreate = (newTodo) => {
-    api.todos.create(newTodo).then(fetchTodos);
+  const handleCreate = async (newTodo) => {
+    try {
+      await api.todos.create(newTodo);
+      await fetchTodos();
+    } catch (error) {
+      console.log("Failed to get todo's. Please try again.");
+    }
   };
 
-  const handleUpdate = (id, newTodo) => {
-    api.todos.update(id, newTodo).then(fetchTodos);
+  const handleUpdate = async (id, newTodo) => {
+    try {
+      await api.todos.update(id, newTodo);
+      await fetchTodos();
+    } catch (error) {
+      console.log("Failed to create todo's. Please try again.");
+    }
   };
 
-  const handleDelete = (id) => {
-    api.todos.delete(id).then(fetchTodos);
+  const handleDelete = async (id) => {
+    try {
+      await api.todos.delete(id);
+      await fetchTodos();
+    } catch (error) {
+      console.log("Failed to delete todo's. Please try again.");
+    }
   };
 
   return (
